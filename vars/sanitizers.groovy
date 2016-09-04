d887bbbc7724e5802a2caa056a68ef00bd44569e
Stephen Newell <sjnewell@gmail.com>
2016-09-03 23:16:25 -0600

    Create a build configuration for launching sanitizer builds

---
+++ vars/sanitizers.groovy
@@ -0,0 +1,33 @@
+// Launch builds for each of the supported sanitizers.  sanitizer will wait
+// until each of the sanitizer builds finishes before reporting success or
+// failure.
+//
+// DEPENDENCIES:
+//   (none)
+//
+// USAGE:
+//   You'll need a Jenkins build that builds using a sanitizer passed as a
+//   parameter.
+//
+// EXAMPLE:
+//  sanitizers {
+//    sanitizerBuild = 'some-sanitizer-build'
+//    sanitizerParam = 'sanitizer'
+//  }
+//
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    parallel address: {
+        build job: config.sanitizerBuild, parameters: [string(name: config.sanitizer, value: 'address')]
+    }, thread: {
+        build job: config.sanitizerBuild, parameters: [string(name: config.sanitizer, value: 'thread')]
+    }, udef: {
+        build job: config.sanitizerBuild, parameters: [string(name: config.sanitizer, value: 'undefined')]
+    }
+}
