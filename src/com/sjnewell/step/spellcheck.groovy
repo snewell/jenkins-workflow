eb38fd7e5baabce329f2206c8b79aa58e56eb7c3
Stephen Newell <sjnewell@gmail.com>
2017-10-14 23:00:02 -0600

    Get flexibleBuild working with spellcheck step

---
+++ src/com/sjnewell/step/spellcheck.groovy
@@ -0,0 +1,29 @@
+
+def execute(args) {
+    stage('Spellcheck') {
+        def modeArg = ''
+        if(args.containsKey('mode')) {
+            modeArg = "--mode=${args.mode}"
+        }
+        def currentDir = pwd()
+        def cmd = "while read f; do " +
+                      "cat \${f} |" +
+                      "aspell list ${modeArg} -p ${pwd()}/${args.dictionary} |" +
+                      "sed \"s,^\\(.*\\),\\1     \${f},g\";" +
+                   "done | " +
+                   "sort | " +
+                   "uniq -c |" +
+                   "sort -r"
+
+        def count = 1
+        args.spellcheckPatterns.each { pattern ->
+            // there's probably a less-hacky way to do this
+            def outfile = "spellcheck.out.${count}"
+            sh "echo ${pattern} >${outfile}"
+            sh "echo >>${outfile}"
+            sh "find ${pattern} | ${cmd} >>${outfile}"
+            ++count
+            archiveArtifacts outfile
+        }
+    }
+}
---
+++ vars/flexibleBuild.groovy
@@ -0,0 +1,13 @@
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def build = new com.sjnewell.genericBuild()
+    def data = build.buildMap(config)
+
+    build.run(data, config.extraSteps)
+}
