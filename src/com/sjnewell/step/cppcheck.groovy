961911bb5983c8033c58632e310a6450a6707744
Stephen Newell <sjnewell@gmail.com>
2017-11-02 21:08:45 -0600

    Strip progress messages from cppcheck output

--- src/com/sjnewell/step/cppcheck.groovy
+++ src/com/sjnewell/step/cppcheck.groovy
@@ -12,7 +12,15 @@ package com.sjnewell.step;
 def call(args) {
     stage('cppcheck') {
         def outputFile = 'cppcheck-output'
-        sh "cppcheck --enable=all --project=${args.buildDir}/compile_commands.json >${outputFile}"
+        try {
+            sh "cppcheck --enable=all --project=${args.buildDir}/compile_commands.json |" +
+               "grep -v '^Checking' |" +
+               "grep -v '^[[:digit:]][[:digit:]]*/[[:digit:]][[:digit:]]*' >${outputFile}"
+        }
+        catch(err) {
+            // cppcheck complains about include paths when piped for some reason, but the
+            // output still seems fine
+        }
         archiveArtifacts outputFile
     }
 }
